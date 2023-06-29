ab72c81ce9444cddf0da84612a558e3e7540ac6b
Zeeshan Shah <zeeshan_shah200750@yahoo.com>
2023-06-29 19:31:28 +0000

    Add function load_records to open records file

--- run.py
+++ run.py
@@ -1,3 +1,16 @@
 # Import required libraries
 import json  # Library for JSON operations
 
+def load_records():
+    """
+    Load records from a JSON file.
+
+    Returns:
+        list: List of records.
+    """
+    try:
+        with open('records.json', 'r') as file:
+            records = json.load(file)  # Load JSON data from file
+    except FileNotFoundError:
+        records = []  # Create an empty list if file not found
+    return records
