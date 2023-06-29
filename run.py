ecb33393bc69c97ec2f7319414d1a8d4b838312a
Zeeshan Shah <zeeshan_shah200750@yahoo.com>
2023-06-29 19:42:20 +0000

    Add function save_records to save records file

--- run.py
+++ run.py
@@ -14,3 +14,15 @@ def load_records():
     except FileNotFoundError:
         records = []  # Create an empty list if file not found
     return records
+
+def save_records(records):
+    """
+    Save records to a JSON file.
+
+    Args:
+        records (list): List of records.
+    """
+    with open('records.json', 'w') as file:
+        json.dump(records, file, indent=4)  # Write JSON data to file with indentation
+
+        
