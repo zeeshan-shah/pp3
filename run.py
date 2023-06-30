d86966bccb8a3db2de826d25f59ac39bb8053c52
Zeeshan Shah <zeeshan_shah200750@yahoo.com>
2023-06-30 20:56:51 +0000

    Add functioadd_record and add input fields

--- run.py
+++ run.py
@@ -25,4 +25,20 @@ def save_records(records):
     with open('records.json', 'w') as file:
         json.dump(records, file, indent=4)  # Write JSON data to file with indentation
 
-        
+def add_record(records):
+    """
+    Add a new record to the HRIS.
+
+    Args:
+        records (list): List of records.
+    """
+    # Get input for each field of the record
+    name = input("Enter the employee full name: ")
+    date_of_birth = get_valid_date("Enter the employee's date of birth (DD-MM-YYYY): ")
+    age = get_valid_input("Enter the employee's age (18 or above): ", int, lambda x: x >= 18)
+    address = input("Enter the employee's address: ")
+    email = get_valid_email("Enter the employee's email address: ")
+    job_position = input("Enter the job position: ")
+    department = input("Enter the department: ")
+    salary = get_valid_input("Enter the employee's salary: ", float, lambda x: x >= 0)
+    hire_date = get_valid_date("Enter the employee's hire date (DD-MM-YYYY): ")        
